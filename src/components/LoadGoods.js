f28829d0f341ce6587bba65ac565c11c0cdf7e2d
jin <e.kardash@seuslab.ru>
2024-05-25 15:44:30 +0500

    Вернул список продуктов из fakestoreapi и сделал обратку ошибки АПИ

--- src/components/LoadGoods.js
+++ src/components/LoadGoods.js
@@ -1,5 +1,14 @@
 
 // GET request using fetch with async/await
-const response = await fetch('https://fakestoreapi.com/products');
-const data = await response.json();
+import {goodsValues} from "./LocalGoods.js";
+let ret = null
+try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    ret = await response.json();
+} catch (e) {
+    console.error(e)
+}
+
+// есди АПИ не доступен - взять  локальные данные:
+const data =  ret ? ret : goodsValues
 export { data }
